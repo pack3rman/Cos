82abd7353fb1648032fef5fa81530ab327cc659e
pack3 <pack3rman@gmail.com>
2021-03-03 10:23:09 +0100

    TEST-0003-STATIC-IDs

--- src/main/java/pl/kurs/java/model/request/RequestPeerson.java
+++ src/main/java/pl/kurs/java/model/request/RequestPeerson.java
@@ -2,9 +2,11 @@ package pl.kurs.java.model.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class RequestPeerson {
 
     private String name;
